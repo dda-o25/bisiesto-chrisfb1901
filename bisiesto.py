cacf8cb9c8e4b78536be8ed2f49f39cea09194f3
chrisfb1901 <christian.flores@iteso.mx>
2025-09-19 00:02:05 -0600

    bisiesto_chris

--- bisiesto.py
+++ bisiesto.py
@@ -1,15 +1,26 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Christian Alonso Flores Burgos
+
+Fecha
+19 de septiembre de 2025
+
+Determinar si un año es bisiesto o no.
+
 """
 
 # Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
+año= int(input("Introduzca un año: "))
 
-# Proceso
-
-
-# Salidas
-print(salida)
+# Proceso 
+if año % 4 != 0:
+    print(año, "no es un año bisiesto")
+else:
+    if año % 100 != 0:
+        print(año, "sí es un año bisiesto")
+    else:
+        if año % 400 == 0:
+            print(año, "sí es un año bisiesto")
+        else:
+            print(año, "no es un año bisiesto")
